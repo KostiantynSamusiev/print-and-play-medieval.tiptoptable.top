11fde6b5a9724da02b8ad459dc29e3e7d4970d24
«KostiantynSamusiev» <djfoozyhook@gmail.com>
2022-07-31 21:20:52 +0300

    first comit

---
+++ js/main.js
@@ -0,0 +1,114 @@
+let mobileLinks = document.querySelectorAll('.mobile-links'),
+     webLinks = document.querySelectorAll('.web-links');
+
+for (let i=0; i<mobileLinks.length; i++)
+mobileLinks[i].onclick = function () {
+    window.scrollTo({
+      left:0,
+      top: 2700,
+      behavior: "smooth"});
+}
+
+for (let i=0; i<webLinks.length; i++)
+webLinks[i].onclick = function () {
+    window.scrollTo({
+      left:0,
+      top: 3700,
+      behavior: "smooth"});
+
+      if (window.screen.width >= 1080) {
+        window.scrollTo({
+          left:0,
+          top: 3100,
+          behavior: "smooth"});
+      }
+}
+
+// Slick-gallery
+$('.gallery').slick({
+    arrows: true,
+    dots: true
+}
+);
+
+ //Button form
+ function closeForm() {
+    $('.ordenary-form').removeClass('is-visible');
+  }
+    $(document).ready(function($) {
+    
+      /* Contact Form Interactions */
+      $('#btnOpenForm').on('click', function(event) {
+        event.preventDefault();
+    
+        $('.ordenary-form').addClass('is-visible');
+      });
+      
+        //close popup when clicking x or off popup
+      $('.ordenary-form').on('click', function(event) {
+        if ($(event.target).is('#btnCloseForm')) {
+          event.preventDefault();
+          $(this).removeClass('is-visible');
+        }
+      });
+    });
+
+    
+// Burger menu
+let icnMenu = document.querySelector('.menu-icon');
+let mobileNav = document.querySelector('.mobile-nav');
+
+icnMenu.addEventListener('click', () => {
+	icnMenu.classList.toggle('active');
+	mobileNav.classList.toggle('is-active');
+});
+
+//Dropdown glags
+$("#country_selector1").countrySelect({
+    defaultCountry: "ua",
+    excludeCountries: ['ru','by'],
+    responsiveDropdown: true,
+    preferredCountries: ["ca", "gb", "us"],
+  });
+
+  $("#country_selector2").countrySelect({
+    defaultCountry: "ua",
+    excludeCountries: ['ru','by'],
+    responsiveDropdown: true,
+  });
+
+    // Automatical download PDF file
+    let pdfUrl = "files/DV_print-and-play.pdf";
+    $('.js-submit-form').on('submit', function (event) {
+        event.preventDefault();
+        const url = $(this).attr('action'),
+        data = $(this).serialize();
+
+
+        $.ajax({
+          type: 'POST',
+          url: url,
+          data: data,
+          success: function(result){
+            window.open(pdfUrl,  '_blank');
+            window.open('thnankyoupage/thank.html', '_self');
+            event.target.reset();
+            $('.ordenary-form').removeClass('is-visible');
+            
+          },
+          error: function(error) {
+          }
+        });
+    });
+
+
+
+   
+   
+
+   
+
+
+   
+
+   
